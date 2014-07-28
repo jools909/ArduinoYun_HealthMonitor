cd40b451b8e03ffc07b82783a9cdaa7cec1061be
Jools <j.ostmo@ucl.ac.uk>
2014-07-28 20:08:57 +0100

    Updated Main.py

--- Python/MScProject/src/Main.py
+++ Python/MScProject/src/Main.py
@@ -1,6 +1,7 @@
 import os
 
 datafolderPath = '***path of folder that contains data files on sd card***'
+dbTableName = 'Sensor1Table'
 
 #Get list of files and sort resultant list
 fileList = os.listdir(datafolderPath)
@@ -8,8 +9,22 @@ fileList.sort()
 
 #loop through each file in list
 for fileOb in fileList:
-    sqlString =""
+    sqlString ="INSERT INTO " + dbTableName + " (DateTime, Reading1) VALUES"
     f=open(fileOb, 'r')
     #go through each line of the current file
     for line in f:
-        
+        sqlString = sqlString + " ('"
+        #loop through each character of the line string
+        workingValue = ''
+        for letter in line:
+            if letter == '=':
+                sqlString = sqlString + workingValue + "',"
+                workingValue = ''
+            elif letter == ',':
+                sqlString = sqlString + workingValue + ","
+                workingValue = ''
+            elif letter == ';':
+                sqlString = sqlString + workingValue + ")"
+                workingValue = ''
+            else:
+                workingValue = workingValue + letter
