33d1cc97e7b6d3c876933d38613a7175a569e929
Jools <j.ostmo@ucl.ac.uk>
2014-08-04 15:02:41 +0000

    Updated DatabaseConnect.py@

--- Python/MScProject/src/DatabaseConnect.py
+++ Python/MScProject/src/DatabaseConnect.py
@@ -12,7 +12,7 @@ class DatabaseConnect:
         #Tries to connect to the cloud database
         #Returns a connection variable if connects, else returns false.
         try:
-            conn = pyodbc.connect('DRIVER={SQL Server Native Client 10.0};SERVER=h9flrbpsdu.database.windows.net;DATABASE=SensorReadingsDB;UID=jools909@h9flrbpsdu;PWD=SPL_ash2')
+            conn = pyodbc.connect('DNS=h9flrbpsdu;UID=jools909@h9flrbpsdu;PWD=SPL_ash2')
         except pyodbc.Error:
             return False
         else:
