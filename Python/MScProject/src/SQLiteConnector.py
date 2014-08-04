a456edf015a8038d390e9a9fa376bae9e6d71680
Jools <j.ostmo@ucl.ac.uk>
2014-08-04 18:55:04 +0000

    Updated SQLiteConnector to allow multiple arguments.

--- Python/MScProject/src/SQLiteConnector.py
+++ Python/MScProject/src/SQLiteConnector.py
@@ -9,15 +9,19 @@ import sys
 import os
 
 databasePath = '/home/azureuser/Documents/SensorReadingsFileLog.db'
-filePath = sys.argv[1]
-sqlString = "INSERT INTO ReadingFiles (FilePath, Uploaded) VALUES ('" + filePath + "', 0)"
 
-if (os.path.isfile(databasePath)):
-    dbConnection = sqlite3.connect(databasePath)
-    try:
-        with dbConnection:
-            dbConnection.execute(sqlString)
-    except sqlite3.IntegrityError as e:
-        print "SQL Integrity Error, INSERT statement failed, rolling back transaction"
-    finally:
-        dbConnection.close()
+#Loop through available process arguments
+for i in range(1,len(sys.argv)):
+    filePath = sys.argv[i]
+    sqlString = "INSERT INTO ReadingFiles (FilePath, Uploaded) VALUES ('" + filePath + "', 0)"
+    if (os.path.isfile(databasePath)):
+        dbConnection = sqlite3.connect(databasePath)
+        try:
+            with dbConnection:
+                dbConnection.execute(sqlString)
+        except sqlite3.IntegrityError as e:
+            print "SQL Integrity Error, INSERT statement failed, rolling back transaction"
+        finally:
+            dbConnection.close()
+    else:
+        print "SensorReadingsFileLog.db not found."
