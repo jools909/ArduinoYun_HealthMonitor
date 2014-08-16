233fa3e06e6aa98a9fbb7ef238c4789f4cf49706
Jools <j.ostmo@ucl.ac.uk>
2014-08-16 15:36:37 +0000

    Updated Cloud database connecter, ArchivedDataCheck script tested and working.

--- Python/MScProject/src/ArchivedDataCheck.py
+++ Python/MScProject/src/ArchivedDataCheck.py
@@ -34,11 +34,12 @@ for fileOb in fileList:
         #If non-uploaded readings are found, an SQL INSERT string is created.
         sqlString = ''
         if results != None:
-            sqlString = "INSERT INTO " + cloudDBtableName + " (DateTime, Reading1) VALUES"
+            sqlString = "INSERT INTO " + cloudDBtableName + " (DateTime, Reading1) values"
             for rows in results:
                 sqlString += " ('%s', %s)," % (rows['DateTime'], rows['Reading'])
             sqlString = sqlString[:-1]
-        
+            sqlString = str(sqlString)
+            
             #If internet connection is available connect to Cloud database
             #and execute INSERT SQL string.
             wifi = WiFiConnection.WiFiConnection()
@@ -49,12 +50,14 @@ for fileOb in fileList:
                     print 'Waiting for WiFi connection'
                 else:
                     print '.'
+                #If no internet connection sleep for 2 seconds,
+                #increasing to a maximum of 60 seconds.
                 time.sleep(2*sleepCounter)
                 if sleepCounter < 30:
                     sleepCounter += 1
-                #wifi = WiFiConnection.WiFiConnection.isConnected()
             print 'WiFi available.'
             
+            #Loop if data upload fails at any point.
             uploadSuccess = False
             while uploadSuccess == False:
                 cloudConnect = DatabaseConnect.DatabaseConnect()
@@ -62,25 +65,28 @@ for fileOb in fileList:
                 if cloudDB != False:
                     print 'Connected to cloud database.'
                     cloudCursor = cloudDB.cursor()
+                    #Try INSERT query of data obtained from SQLite database
                     try:
                         cloudCursor.execute(sqlString)
                         cloudDB.commit()
-                        uploadSuccess == True
+                        uploadSuccess = True
                         print 'Executed INSERT SQL.'
                     except pyodbc.Error as e:
                         print e.args
-                    cloudDB.close()
+                    finally:
+                        cloudDB.close()
                 else:
                     print 'Failed to connect to cloud database'
                     time.sleep(1)
+                    cloudDB = cloudConnect.Connect()
             
             #Update SQlite .db fileOb so that 'Uploaded' == True (1)
-            if uploadSuccess:
-                SQLiteDBupdated = False
-                while SQLiteDBupdated == False:
-                    updateResult = db.Insert("UPDATE Readings SET Uploaded = 1 WHERE Uploaded = 0")
-                    if updateResult:
-                        SQLiteDBupdated = True
-                        print 'SQLite database updated.'
+            SQLiteDBupdated = False
+            while SQLiteDBupdated == False:
+                updateResult = db.Insert("UPDATE Readings SET Uploaded = 1 WHERE Uploaded = 0")
+                if updateResult:
+                    SQLiteDBupdated = True
+                    print 'SQLite database ' + fileOb + ' updated.'
         else:
             print 'All readings in ' + fileOb + ' have already been uploaded.'
+print 'Archived data fully uploaded.' 
