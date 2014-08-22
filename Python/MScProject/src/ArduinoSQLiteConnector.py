37f83b8eebd1e32d3193d7750ee77bcdcdaadeb5
Jools <j.ostmo@ucl.ac.uk>
2014-08-22 17:56:01 +0000

    Updated Arduino SQLite Connector script.

--- Python/MScProject/src/ArduinoSQLiteConnector.py
+++ Python/MScProject/src/ArduinoSQLiteConnector.py
@@ -10,6 +10,9 @@ Script run by the Arduino to INSERT sensor readings into the SQLite database.
 import sys
 import SQLiteDatabaseHandler
 
-sqlString = sys.argv[1]
+SQLiteDBpath = '/mnt/sd/SQLiteDatbases/'
+SQLiteDatabase = sys.argv[1]
+sqlString = sys.argv[2]
 
-sqliteDB = SQLiteDatabaseHandler.SQLiteDatabaseHandler()
+sqliteDB = SQLiteDatabaseHandler.SQLiteDatabaseHandler(SQLiteDBpath + SQLiteDatabase)
+sqliteDB.Insert(sqlString)
